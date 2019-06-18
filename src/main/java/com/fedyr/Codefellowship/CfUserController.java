84d64b873289f2e23415ac3adbe03336f660eb87
Paula Thomas <paula9t9@gmail.com>
2019-06-18 12:54:23 -0700

    Basic login initialized

--- src/main/java/com/fedyr/Codefellowship/CfUserController.java
+++ src/main/java/com/fedyr/Codefellowship/CfUserController.java
@@ -6,10 +6,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.security.Principal;
 import java.util.ArrayList;
 
 @Controller
@@ -20,6 +22,12 @@ public class CfUserController {
 
     @Autowired
     PasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/")
+    public String getHome(Principal p, Model m){
+        m.addAttribute("principal", p);
+        return "codefellowship";
+    }
 
     @PostMapping("/users")
     public RedirectView createUser(String username, String password){
--- src/main/java/com/fedyr/Codefellowship/CodefellowshipController.java
+++ src/main/java/com/fedyr/Codefellowship/CodefellowshipController.java
@@ -11,5 +11,8 @@ public class CodefellowshipController {
         return "codefellowship";
     }
 
-
+    @GetMapping("/signup")
+    public String getSignup(){
+        return "signup";
+    }
 }
